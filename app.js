711bd34ff43ef6aaf5ffae9664147f4439912d7a
hz4206 <nitin.sanghi@gmail.com>
2014-03-03 13:49:10 +0200

    Changed Index.jade

--- app.js
+++ app.js
@@ -28,8 +28,47 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+var quotes = [
+    { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
+    { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
+    { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
+    { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
+];
+
 app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/users', user.list);
+
+app.get('/quotes/list',function(req,res){
+    res.json(quotes);
+});
+app.get('/quotes/random',function(req,res){
+    var id = Math.floor(Math.random() * quotes.length);
+    var q = quotes[id];
+    res.json(q);
+});
+
+app.get('/quotes/:id',function(req,res){
+    if(quotes.length <= req.params.id || req.params.id < 0){
+        res.statusCode = 404;
+        return res.send('Error 404: No Quote Found');
+    }
+    var q = quotes[req.params.id];
+    res.json(q);
+});
+
+app.post('/quotes',function(req,res){
+    if(!req.body.hasOwnProperty('author') || !req.body.hasOwnProperty('text')) {
+        res.statusCode = 400;
+        return res.send('Error 400: Post syntax incorrect.');
+    }
+
+    var newQuote = {
+        author : req.body.author,
+        text : req.body.text
+    };
+    quotes.push(newQuote);
+    res.json(true);
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
